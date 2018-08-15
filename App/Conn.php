dcacd3b01d95425db225d48fefcd75a2bc0b0fd0
Rossano <rossanorb@gmail.com>
2018-08-15 19:03:29 -0300

    fix conexao com docker mysql

--- App/Conn.php
+++ App/Conn.php
@@ -6,6 +6,6 @@ class Conn
 {
     public static function getDb()
     {
-        return new \PDO("mysql:host=localhost;dbname=modelo","root","102030");
+        return new \PDO("mysql:host=127.0.0.1;dbname=controle_estoque","root","102030");
     }
 }
